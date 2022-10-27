edde08372e911f8a4f3050d52831d83db19ad6cb
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-10-27 19:23:04 -0400

    creating call to our roll function

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,18 @@
+#!/usr/bin/env node
 
+// import stuff
+import minimist from 'minimist'
+import { roll } from "/lib/roll.js"
+
+// arg const
+const args = minimist(process.argv.slice(2))
+
+// create constants for sides, dice, and rolls. Default values included.
+const sides = args.sides ? args.sides: 6;
+const dice = args.dice ? args.dice: 2;
+const rolls = args.rolls ? args.rolls: 1;
+
+// print out the results using stringify
+console.log(JSON.stringify(roll(sides, dice, rolls)));
+// all done!
+process.exit(0);
